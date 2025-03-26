166a29c0c06c018141a79a3b758d079a14b78dcf
Greeshma G <greeshmag275@gmail.com>
2025-03-26 07:40:57 +0530

    changed

--- components/CallToAction.js
+++ components/CallToAction.js
@@ -1,3 +1,4 @@
+'use client';
 import { useEffect } from 'react';
 import styles from '../styles/CallToAction.module.css';
 
--- components/SolutionsSection.js
+++ components/SolutionsSection.js
@@ -1,3 +1,4 @@
+'use client';
 import { useEffect } from 'react';
 import styles from '../styles/SolutionsSection.module.css';
 
